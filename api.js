02765740d8eb48eb5528580c935a17631fb4901c
MATRune <malik.thomas.200531@gmail.com>
2025-04-16 12:07:23 -0400

    Update api.js

---
+++ api.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=304554';
+const options = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': '4b34154c75msha7b904a0e8c010dp12510djsn96d217884c5d',
+    'x-rapidapi-host': 'tripadvisor16.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
