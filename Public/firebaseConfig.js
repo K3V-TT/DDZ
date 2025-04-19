82c142145489251677d352ed663f45a4e0af8bbf
MATRune <malik.thomas.200531@gmail.com>
2025-04-18 20:41:44 -0400

    Create firebaseConfig.js

---
+++ Public/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLxERsk-s3AI23xg4dMOVW-7SqgUqraOk",
+  authDomain: "digital-deal-zone-153ee.firebaseapp.com",
+  projectId: "digital-deal-zone-153ee",
+  storageBucket: "digital-deal-zone-153ee.firebasestorage.app",
+  messagingSenderId: "522233837844",
+  appId: "1:522233837844:web:8a0368a3dfed1a47feaa01"
+};
+/*
+Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
+export default firebaseConfig
